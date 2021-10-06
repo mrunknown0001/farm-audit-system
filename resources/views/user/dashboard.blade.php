c4827c859159df23a29d99c3c8872295a4aefd8f
Adam <madamt0001@gmail.com>
2021-10-06 22:31:11 +0800

    Update

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -5,7 +5,17 @@
 @endsection
 
 @section('style')
+<style>
+	.image-upload > input
+	{
+		display: none;
+	}
 
+	.image-upload i
+	{
+	  cursor: pointer;
+	}
+</style>
 @endsection
 
 @section('sidebar')
@@ -29,11 +39,16 @@
 		</div>
 		<div class="row">
 			<div class="col-md-6">
-				<form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data">
+				<form action="{{ route('upload') }}" id="imageUploadForm" method="POST" enctype="multipart/form-data">
 					@csrf
-					<input type="file" id="upload" name="upload" accept="image/*" capture>
-					<br>
-					<button type="submit">Up</button>
+					<div class="image-upload">
+						<input type="file" id="upload" name="upload" accept="image/*" capture>
+						<label for="upload">
+							<span id="camera"><i class="fa fa-camera fa-3x"></i></span>
+						</label>
+						{{-- <button type="submit" disabled="" id="submit"><i class="fa fa-upload fa-3x"></i></button> --}}
+					</div>
+					
 				</form>
 			</div>		
 		</div>
@@ -49,5 +64,56 @@
 @endsection
 
 @section('script')
+<script type="text/javascript">
+  $(function() {
+     $("#upload").change(function (){
+       $("#submit").attr('disabled', false);
+       $("#camera").addClass('btn btn-success');
+       $("#submit").addClass('btn btn-success');
+     });
+  });
 
+$(document).ready(function (e) {
+  $('#imageUploadForm').on('submit',(function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+
+    $.ajax({
+      type:'POST',
+      url: $(this).attr('action'),
+      data:formData,
+      cache:false,
+      contentType: false,
+      processData: false,
+      success:function(data){
+        console.log("success");
+        // console.log(data);
+        Swal.fire({
+          title: 'Image Uploaded!',
+          text: "",
+          type: 'success',
+          showCancelButton: false,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Close'
+        });
+      },
+      error: function(data){
+        console.log("error");
+        // console.log(data);
+	      Swal.fire({
+				  type: 'error',
+				  title: 'Error Occured',
+				  text: 'Please Try Again.',
+				  // footer: '<a href="">Why do I have this issue?</a>'
+				});
+      }
+    });
+  }));
+
+  $("#upload").on("change", function() {
+    $("#imageUploadForm").submit();
+  });
+});
+</script>
 @endsection
