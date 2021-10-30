a047bce370b916958e11914776e9c62d5b0e24d6
Adam <madamt0001@gmail.com>
2021-10-31 05:52:08 +0800

    update

--- resources/views/includes/common/review/review.blade.php
+++ resources/views/includes/common/review/review.blade.php
@@ -5,6 +5,9 @@
 @endsection
 
 @section('style')
+	<link href="https://cdn.bootcss.com/balloon-css/0.5.0/balloon.min.css" rel="stylesheet">
+  <link href="{{ asset('magnify/css/jquery.magnify.css') }}" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{ asset('summernote/summernote.min.css') }}">
 @endsection
 
 @section('sidebar')
@@ -47,12 +50,33 @@
 				@if($audit->compliance == 1) 
 					<p><span class="label label-success">COMPLIANT <i class="fa fa-check"></i></span></p>
 				@else
-					<p><span class="label label-danger">NON-COMPLIANT <i class="fa fa-close"></i></span></p>
+					<p><span class="label label-danger">NON-COMPLIANT <i class="fa fa-times"></i></span></p>
 				@endif
 				<p><a href=""><i class="fa fa-map-marked-alt"></i> View Location</a></p>
 				<p>Timestamp: {{ $audit->created_at }}</p>
 				<hr>
 				<p>Audit Item: <strong>{{ $audit->audit_item->item_name . ' (' . $audit->audit_item->time_range . ')' }}</strong></p>
+				@if($audit->compliance == 0)
+					<p>Bakit hindi Compliant?</p>
+					<p><i>{{ $audit->non_compliance_remarks }}</i></p>
+					@if($audit->field2 != null)
+						<p>Additional Remarks</p>
+						<p><i>{{ $audit->field2 }}</i></p>
+					@endif
+					@if(count($audit->images) > 0)
+						<div class="image-set m-t-20">
+							@foreach($audit->images as $i)
+								<a data-magnify="gallery" data-src="" data-caption="{{ $i->filename }}" data-group="a" href="{{ asset('/uploads/images/' . $i->filename) }}">
+				          <img src="{{ asset('/uploads/images/' . $i->filename) }}" alt="" height="50px">
+				        </a>
+							@endforeach
+						</div>
+					@endif
+				@endif
+				<hr>
+				<h4>Reviewer Remarks</h4>
+				<textarea id="summernote" name="editordata"></textarea>
+				<button class="btn btn-primary"><i class="fa fa-save"></i> Save</button>
 			</div>
 		</div>
 	</section>
@@ -60,5 +84,53 @@
 @endsection
 
 @section('script')
+	<script src="https://cdn.bootcss.com/prettify/r298/prettify.min.js"></script>
+	<script src="{{ asset('magnify/js/jquery.magnify.js') }}"></script>
+	<script src="{{ asset('summernote/summernote.min.js') }}"></script>
+  <script>
+		$(document).ready(function() {
+		 $('#summernote').summernote({
+        placeholder: 'Reviewer Remarks...',
+        tabsize: 2,
+        height: 150,
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['table', ['table']],
+          ['insert', []]
+          // ['insert', ['link', 'picture', 'video']],
+          ['view', ['fullscreen', 'help']]
+        ]
+		 });
+		});
+    window.prettyPrint && prettyPrint();
 
+    var defaultOpts = {
+      draggable: true,
+      resizable: true,
+      movable: true,
+      keyboard: true,
+      title: true,
+      modalWidth: 320,
+      modalHeight: 320,
+      fixedContent: true,
+      fixedModalSize: false,
+      initMaximized: false,
+      gapThreshold: 0.02,
+      ratioThreshold: 0.1,
+      minRatio: 0.05,
+      maxRatio: 16,
+      headToolbar: ['maximize', 'close'],
+      footToolbar: ['zoomIn', 'zoomOut', 'prev', 'fullscreen', 'next', 'actualSize', 'rotateRight'],
+      multiInstances: true,
+      initEvent: 'click',
+      initAnimation: true,
+      fixedModalPos: false,
+      zIndex: 1090,
+      dragHandle: '.magnify-modal',
+      progressiveLoading: true
+    };
+  </script>
 @endsection
