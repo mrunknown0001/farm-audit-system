7974f565691605bfc39fa09a37d0f517fda0947a
Adam <madamt0001@gmail.com>
2021-11-01 18:44:16 +0800

    update

--- resources/views/includes/common/review/review.blade.php
+++ resources/views/includes/common/review/review.blade.php
@@ -113,9 +113,22 @@
         ]
 		 });
 
-			var latlon = {{ $audit->latitude }} + "," + {{ $audit->longitude }};
-			var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyAQvHBXoM12klgegEIh1rTfklVQR3XkAXw";
-			document.getElementById("mapholder").innerHTML = "<img class='img img-responsive' src='"+img_url+"'>";
+			var lat = {{ $audit->latitude }};
+			var lon = {{ $audit->longitude }};
+			// var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyAQvHBXoM12klgegEIh1rTfklVQR3XkAXw";
+		  var latlon=new google.maps.LatLng(lat, lon)
+		  var mapholder=document.getElementById('mapholder')
+		  mapholder.style.height='250px';
+		  mapholder.style.width='100%';
+
+		  var myOptions={
+		  center:latlon,zoom:14,
+		  mapTypeId:google.maps.MapTypeId.ROADMAP,
+		  mapTypeControl:false,
+		  navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+		  };
+		  var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
+		  var marker=new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
 
 			$("#showmap").click(function () {
 				$("#mapholder").show();
