ba2174b75af3139355acc0f9d1f546b6d83089ee
Adam <madamt0001@gmail.com>
2021-11-04 07:42:10 +0800

    Update'

--- resources/views/includes/common/dash-view.blade.php
+++ resources/views/includes/common/dash-view.blade.php
@@ -44,7 +44,7 @@
           <!-- BAR CHART -->
           <div class="box box-danger">
             <div class="box-header with-border">
-              <h3 class="box-title">All Location Current Year Audit: {{ date('Y') }}</h3>
+              <h3 class="box-title">All Location Current Year Audit: {{ date('Y') }} (sample data)</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
